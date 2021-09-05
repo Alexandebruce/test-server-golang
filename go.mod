0983f45daf9367e4e4338c5a9c5f0256fc943a1c
Fedotov Alexander <bruce2013@yandex.ru>
2021-09-05 18:21:33 +0300

    begin
    fix

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Alexandebruce/test-server-golang
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.7.2
+)
---
+++ handlers/ping_handler.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"github.com/Alexandebruce/test-server-golang/db/logging"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func PingHandler(logger *logging.Repository) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if _, err := io.WriteString(w, "I'm sms-service"); err != nil {
+			log.Printf("error to write response: %s", err)
+		}
+
+		if err := logger.Insert(logging.PingLogMessage{
+			Message: "Ping",
+			Date: time.Now(),
+		}); err != nil {
+			log.Printf("error to write log message: %s", err)
+		}
+	})
+}
